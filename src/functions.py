cd9a5d9abc351937d424948592509911e41c0ffb
jrduncan831 <jrduncan831@gmail.com>
2020-06-28 12:10:28 -0500

    update

--- src/functions.py
+++ src/functions.py
@@ -1,3 +1,6 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
 Code_to_text = {'H':"HER",'O':"ORR","S":"Slab","N":"Nanoparticle"}
 target_BE = {'O':-1.143,'H':-0.4915} # Ideal binding energies for ORR ('O')and HER ('H'
 
@@ -35,3 +38,55 @@ def print_material_from_key(key):
     '''
     key_split = key.split('/')
     print(key_split[1] + key_split[2] + key_split[3] + key_split[4] +' '+Code_to_text[key_split[0]] + ' for ' + Code_to_text[key_split[5]])
+
+# Plot binding energy versus ensemble 
+
+def get_BE_AAA_and_AAB(df, metalA,metalB,material_type,reaction):
+    """
+    Returns binding energy + std for AAA and AAB ensemble for metal combination
+    """
+    ensemble_array,std_array = np.zeros(2),np.zeros(2)
+    for i,ensemble in enumerate(['AAA','AAB']):
+        key = get_keys_for_material(df, metalA,metalB,material_type,reaction,ensemble)
+        if key != None:
+            ensemble_array[i],std_array[i] = df.loc[key,'result'],df.loc[key,'std']
+        else:
+            return None,None 
+    return ensemble_array,std_array 
+
+    
+
+def get_BE_with_changing_ensemble(df, metalA,metalB,material_type,reaction):
+    """
+    Returns binding energy array for four possible ensembles and cooresponding standard deviation
+    """
+    be_array,std_array = np.zeros(4),np.zeros(4)
+    be_array[:2], std_array[:2] = get_BE_AAA_and_AAB(df, metalA,metalB,material_type,reaction)
+    be_array[2:], std_array[2:] = get_BE_AAA_and_AAB(df, metalB,metalA,material_type,reaction)
+    be_array[2],be_array[3] = be_array[3],be_array[2]
+    std_array[2],std_array[3] = std_array[3],std_array[2]
+    return be_array, std_array
+
+def plot_BE_change_with_ensemble(df, metalA,metalB,material_type,reaction,show_pure=False):
+    """
+    Parameters:
+    show_pure - Set to True if you want to show pure binding energies of two metals
+    """
+    num_metal_A = np.linspace(0,3,4)
+    fig, ax = plt.subplots()
+    be_array, std_array = get_BE_with_changing_ensemble(df, metalA,metalB,material_type,reaction)
+    ax.scatter(num_metal_A,be_array,label= metalA + metalB + ' binding energy')
+    ax.errorbar(num_metal_A,be_array,std_array)
+    if show_pure:
+        metalA_be = df.loc[get_pure_key(metalA, material_type, reaction),'result']
+        metalB_be = df.loc[get_pure_key(metalB, material_type, reaction),'result']
+        ax.plot(num_metal_A,metalA_be * np.ones(len(num_metal_A)),label=metalA + ' binding energy')
+        ax.plot(num_metal_A,metalB_be * np.ones(len(num_metal_A)),label=metalB + ' binding energy')
+    ax.legend()
+    ax.set_xticks(num_metal_A)
+    ax.set_xticklabels([3*metalA,(2*metalA)+metalB, metalA+(2*metalB),3*metalB])
+    ax.set_xlabel('Ensemble')
+    ax.set_ylabel('Average Binding Energy (eV)')
+    ax.set_title("Binding Energy versus Ensemble of "+ metalA + metalB + ' '+ Code_to_text[material_type] +" for the "+Code_to_text[reaction])
+
+
